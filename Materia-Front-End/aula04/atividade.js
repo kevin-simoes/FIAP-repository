3e54eeac7963374b78945bd312703a2e333db567
Kevin Simões <kevinsimoeslima@gmail.com>
2026-03-23 11:47:33 -0300

    :feat: add aula04 files

---
+++ Materia-Front-End/aula04/atividade.js
@@ -0,0 +1,8 @@
+var nome = 'Kevin'
+var sobrenome = 'Simões'
+var ano = '2026'
+var idade = '18'
+
+var nascimento = ano - idade;
+
+console.log(`Me chamo ${nome} ${sobrenome}, nasci no ano de ${nascimento}`)
