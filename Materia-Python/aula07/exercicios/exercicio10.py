8fe707f6096cce11be7d0de85c0bd50004e44a88
Kevin Simões <kevinsimoeslima@gmail.com>
2026-03-27 11:38:24 -0300

    fix: logical of exercicio10 file

--- Materia-Python/aula07/exercicios/exercicio10.py
+++ Materia-Python/aula07/exercicios/exercicio10.py
@@ -1,14 +1,15 @@
 salario = int(input('Digite o salário fixo: '))
 vendas = int(input('Digite o valor das vendas: '))
 
-comissao = vendas * 0.03
-
-if comissao > 1500:
+if vendas > 1500:
     p5 = vendas - 1500
-    p3 = (vendas * 0.03) - p5
-    comissao == (p5 * 0.05) + p3
+    p3 = 1500 * 0.03
+    vendas = (p5 * 0.05)+p3
 
-total = comissao + salario
+    total = vendas + salario
+
+else:
+    total = (vendas * 0.03) + salario
 
 print(total)
 
