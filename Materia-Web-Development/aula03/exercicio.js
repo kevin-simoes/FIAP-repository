7633e29b0d621cb601a748c8caf446ec21948407
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-30 11:24:52 -0300

    feat: a frase q ce quer

---
+++ Materia-Web-Development/aula03/exercicio.js
@@ -0,0 +1,6 @@
+let nome = prompt('Digite o seu nome:');
+let numero1 = parseInt(prompt('Digite um número:'));
+let numero2 = parseInt(prompt('Digite um número:'));
+
+console.log(`${nome}, a soma dos dois número é ${numero1 + numero2}`);
+alert(`${nome}, o produto dos dois números é ${numero1 * numero2}`);
---
+++ Materia-Web-Development/aula03/script.js
@@ -0,0 +1,12 @@
+let nome = prompt('Digite seu nome: ')
+
+console.log('Bem-vindo!');
+// console.log('Olá, ' + nome + '!');
+console.log(`Olá ${nome}!`);
+
+alert(`Bem-vindo, ${nome}!`)
+
+let idade = parseInt(prompt('Digite sua idade:'));
+// let idadeInt = parseInt(idade);
+// idade = parseInt(idade);
+alert(`Legal, você terá ${idade + 25} daqui a 25 anos`);
