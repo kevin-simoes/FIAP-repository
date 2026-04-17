3c7a407405ecc851285aa9660132b2ca377888ae
Kevin Simões <kevinsimoeslima@gmail.com>
2026-04-17 11:35:54 -0300

    Fix factorial calculation and update input method

--- Materia-Python/aula11/exercicios/ex7.py
+++ Materia-Python/aula11/exercicios/ex7.py
@@ -1,10 +1,17 @@
-# Faça um algoritmo que solicite um número inteiro ao usuário e calcule o fatorial desse número. O fatorial de um número N é a multiplicação de N por seus antecessores maiores ou iguais a 1.
+# Faça um algoritmo que solicite um número inteiro ao usuário e
+# calcule o fatorial desse número. O fatorial de um número N é a multiplicação
+# de N por seus antecessores maiores ou iguais a 1.
 # Por exemplo: o fatorial de 5 é igual a 120 (5 * 4 * 3 * 2 * 1).
 
-cont = 0
+fatorial = 1
 
-n = 10
-fn = n *
+n = int(input("Digite um número: "))
+cont = n
 
-while cont > n:
-    fn -= 1
+while cont >= 1:
+    fatorial *= cont
+    cont -= 1
+
+print(f"{n}! = {fatorial}")
+
+
