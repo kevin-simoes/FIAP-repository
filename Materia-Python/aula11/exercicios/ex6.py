cff80cb54b2550604bea1c3d75a6e77da17e264b
Kevin Simões <kevinsimoeslima@gmail.com>
2026-04-17 10:51:56 -0300

    Revise algorithm to find the smallest number
    Updated the algorithm to find the smallest number among 10 inputs instead of counting even and odd numbers.

--- Materia-Python/aula11/exercicios/ex6.py
+++ Materia-Python/aula11/exercicios/ex6.py
@@ -1,18 +1,13 @@
 # Escreva um algoritmo que solicite 10 números e informe qual foi o menor número digitado.
 
 cont = 0
-cont_pares = 0
-cont_impares = 0
-num_pares = 0
-num_impares = 0
-while cont < 10:
+
+menor = int(input("Numero: "))
+
+while cont < 9:
     numero = int(input("Numero: "))
-    if numero % 2 == 0:
-        num_pares += numero
-        cont_pares += 1
-    if numero % 2 != 0:
-        num_impares += numero
-        cont_impares += 1
+    if numero > menor:
+        menor = numero
     cont += 1
 
-print(f"")
+print(numero)
