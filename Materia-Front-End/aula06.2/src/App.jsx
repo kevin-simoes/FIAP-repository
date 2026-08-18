983679ed427dbb2fb3b5d8a63916f7f0b6e21f87
Laboratório FIAP <kevinsimoeslima@gmail.com>
2026-08-18 09:10:34 -0300

    feat: adicionando Aluno.jsx no repositorio principal

---
+++ Materia-Front-End/aula06.2/src/App.jsx
@@ -0,0 +1,12 @@
+import Aluno from './components/Aluno'
+
+const NomeDoArquivo = () => {
+  return (
+    //fragments
+    <>
+    <App></App>
+    </>
+  )
+}
+
+export default App
---
+++ Materia-Front-End/aula06.2/src/components/Aluno.jsx
@@ -0,0 +1,10 @@
+const Aluno = () => {
+    return (
+        <>
+        <p>O nome do Aluno é:</p>
+        <p>A Idade do Aluno é:</p>
+        </>
+    )
+}
+
+export default Aluno
